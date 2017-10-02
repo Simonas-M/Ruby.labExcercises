64cdad66024b64d94bf642c6e63c2bb55b050779
SimonasMikulis <simonas.mikulis+gitkraken@gmail.com>
2017-10-02 20:38:12 +0300

    movie genre constants

---
+++ spec/movie/movie_genre_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../../src/movie/movie_genre.rb'
+
+describe '::Movie Genres' do
+  it 'should return true if movie genre exists' do
+    expect(MovieGenres.include?(:action)).to be true
+  end
+  it "should return false if movie genre doesn't exist" do
+    expect(MovieGenres.include?(:walking)).to be false
+  end
+  it 'should accept strings as arguments' do
+    expect(MovieGenres.include?('action')).to be true
+  end
+  it 'should accept strings as arguments' do
+    expect(MovieGenres.include?('action')).to be true
+  end
+  it 'should be frozen' do
+    before = MovieGenres::GENRES
+    expect { MovieGenres::GENRES = 'not genres' }.to raise_error
+    expect(MovieGenres::GENRES).to eq(before)
+  end
+end
