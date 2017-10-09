fa4c5890e6141d8a66cd1abaa73bee861f1f3528
Simonas Mikulis <simonas.mikulis+gitkraken@gmail.com>
2017-10-09 22:28:38 +0300

    movieCrew mutant

--- spec/movie/movie_crew_spec.rb
+++ spec/movie/movie_crew_spec.rb
@@ -1,12 +1,13 @@
 require_relative '../../src/movie/movie_crew.rb'
 
-RSpec.describe '::Movie Crew' do
+RSpec.describe 'MovieCrew' do
   before(:each) do
     @crew_args = {
       directors: %w[Adam John],
       writers: ['George'],
       actors: %w[Billy Eve Cornelia]
     }
+    @movie_crew = MovieCrew.new(@crew_args)
   end
   it 'should initialize' do
     movie_crew = MovieCrew.new(@crew_args)
@@ -14,5 +15,10 @@ RSpec.describe '::Movie Crew' do
   end
   it 'should raise error if no arguments are passed' do
     expect { MovieCrew.new }.to raise_error(ArgumentError)
+  end
+  it 'should get directors' do
+    expect(@movie_crew.directors).to eq %w[Adam John]
+    expect(@movie_crew.writers).to eq ['George']
+    expect(@movie_crew.actors).to eq %w[Billy Eve Cornelia]
   end
 end
