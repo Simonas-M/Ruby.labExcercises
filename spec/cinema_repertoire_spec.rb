70c7a04b8690b5730311a65ce01595446301f033
Simonas Mikulis <simonas.mikulis+gitkraken@gmail.com>
2017-10-31 16:19:24 +0200

    added custom matchers

--- spec/cinema_repertoire_spec.rb
+++ spec/cinema_repertoire_spec.rb
@@ -3,8 +3,10 @@ require_relative '../src/movie/movie_screening.rb'
 require_relative '../src/cinema_repertoire.rb'
 require_relative '../src/cinema_screen.rb'
 require_relative './helpers/movie_helper.rb'
+require_relative './custom_matchers/repertoire_matchers.rb'
 
 RSpec.describe 'CinemaRepertoire' do
+  include RepertoireMatchers
   before(:each) do
     @repertoire = CinemaRepertoire.new
     @screening =  MovieScreening.new(movie: MovieHelper.create,
@@ -35,13 +37,13 @@ RSpec.describe 'CinemaRepertoire' do
 
   it 'should add new screening' do
     @repertoire.add_screening(new_screening: @screening)
-    expect(@repertoire.movie_screenings.include?(@screening)).to be true
+    expect(@repertoire).to have_movie_screening(@screening)
   end
 
   it 'should add multiple screenings' do
     @repertoire.add_screenings(screenings: [@screening, @non_overlaping])
-    expect(@repertoire.movie_screenings.include?(@screening)).to be true
-    expect(@repertoire.movie_screenings.include?(@non_overlaping)).to be true
+    expect(@repertoire).to have_movie_screening(@screening)
+    expect(@repertoire).to have_movie_screening(@non_overlaping)
   end
 
   it 'should delete existing screening' do
@@ -53,7 +55,7 @@ RSpec.describe 'CinemaRepertoire' do
   it 'should raise when trying to delete non existing screening' do
     @repertoire.add_screening(new_screening: @screening)
     @repertoire.del_screening(screening: @screening)
-    expect(@repertoire.movie_screenings.include?(@screening)).to be false
+    expect(@repertoire).not_to have_movie_screening(@screening)
   end
 
   it 'should not add a screening if time overlaps and screens are the same' do
@@ -65,28 +67,26 @@ RSpec.describe 'CinemaRepertoire' do
   it 'should add new screning if time overlap but screens are different' do
     @repertoire.add_screening(new_screening: @screening)
     @repertoire.add_screening(new_screening: @different_screen)
-    expect(@repertoire.movie_screenings.include?(@different_screen))
-      .to be true
+    expect(@repertoire).to have_movie_screening(@different_screen)
   end
 
   it 'should add new screning if time does not overlap' do
     @repertoire.add_screening(new_screening: @screening)
     @repertoire.add_screening(new_screening: @non_overlaping)
-    expect(@repertoire.movie_screenings.include?(@non_overlaping))
-      .to be true
+    expect(@repertoire).to have_movie_screening(@non_overlaping)
   end
 
   it 'should add a new movie' do
     movie = MovieHelper.create
     @repertoire.add_movie(movie: movie)
-    expect(@repertoire.movies.include?(movie)).to be true
+    expect(@repertoire).to have_movie(movie)
   end
 
   it 'should add multiple movies' do
     movie = MovieHelper.create
     @repertoire.add_movies(movies: [movie, @non_overlaping])
-    expect(@repertoire.movies.include?(movie)).to be true
-    expect(@repertoire.movies.include?(@non_overlaping)).to be true
+    expect(@repertoire).to have_movie(movie)
+    expect(@repertoire).to have_movie(@non_overlaping)
   end
 
   it 'should not add same movie' do
@@ -100,7 +100,7 @@ RSpec.describe 'CinemaRepertoire' do
     movie = MovieHelper.create
     @repertoire.add_movie(movie: movie)
     @repertoire.del_movie(movie: movie)
-    expect(@repertoire.movies.include?(movie)).to be false
+    expect(@repertoire).not_to have_movie(movie)
   end
 
   it 'should raise when trying to delete non existing movie' do
@@ -114,12 +114,12 @@ RSpec.describe 'CinemaRepertoire' do
   it 'should not be able to modify movies directly' do
     expect { @repertoire.movies.push(movie: 'movie') }
       .to raise_error(NoMethodError)
-    expect(@repertoire.movies.include?('movie')).to be false
+    expect(@repertoire).not_to have_movie('movie')
   end
 
   it 'should not be able to modify screenings directly' do
     expect { @repertoire.movie_screenings.push(movie: 'movie') }
       .to raise_error(NoMethodError)
-    expect(@repertoire.movie_screenings.include?('movie')).to be false
+    expect(@repertoire).not_to have_movie('movie')
   end
 end
