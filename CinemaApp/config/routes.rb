990192797d2253d1624c2c75033a572c89cb4c84
SimonasMikulis <simonas.mikulis+gitkraken@gmail.com>
2017-12-04 12:11:19 +0200

    added tickets

--- CinemaApp/config/routes.rb
+++ CinemaApp/config/routes.rb
@@ -6,13 +6,19 @@ Rails.application.routes.draw do
   get '/movies', to: 'movies#index'
   get '/movies/new', to: 'movies#new'
   get '/movies/:id', to: 'movies#show', as: 'movie'
-  post '/movies', to: 'movies#create'
+  post '/movies', to: 'movies#create', as: 'new_movie'
   delete '/movies/:id', to: 'movies#destroy'
 
   get '/screenings', to: 'screenings#index'
   get '/screenings/new', to: 'screenings#new'
   get '/screenings/:id', to: 'screenings#show', as: 'screening'
-  post '/screenings', to: 'screenings#create'
+  post '/screenings', to: 'screenings#create', as: 'new_screening'
   delete '/screenings/:id', to: 'screenings#destroy'
+
+  get '/tickets', to: 'tickets#index'
+  get '/tickets/new', to: 'tickets#new'
+  get '/tickets/:id', to: 'tickets#show', as: 'ticket'
+  post '/tickets', to: 'tickets#create', as: 'new_ticket'
+  delete '/tickets/:id', to: 'tickets#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
