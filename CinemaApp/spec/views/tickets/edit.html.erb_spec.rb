7fb2da2eb548804bf3e647ac23bcdcd788d2ab53
SimonasMikulis <simonas.mikulis+gitkraken@gmail.com>
2017-12-05 16:21:37 +0200

    commented out ticket controller and views

--- CinemaApp/spec/views/tickets/edit.html.erb_spec.rb
+++ CinemaApp/spec/views/tickets/edit.html.erb_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "tickets/edit", type: :view do
-  before(:each) do
-    @ticket = assign(:ticket, Ticket.create!())
-  end
+# RSpec.describe "tickets/edit", type: :view do
+#   before(:each) do
+#     @ticket = assign(:ticket, Ticket.create!())
+#   end
 
-  it "renders the edit ticket form" do
-    render
+#   it "renders the edit ticket form" do
+#     render
 
-    assert_select "form[action=?][method=?]", ticket_path(@ticket), "post" do
-    end
-  end
-end
+#     assert_select "form[action=?][method=?]", ticket_path(@ticket), "post" do
+#     end
+#   end
+# end
--- CinemaApp/spec/views/tickets/index.html.erb_spec.rb
+++ CinemaApp/spec/views/tickets/index.html.erb_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "tickets/index", type: :view do
-  before(:each) do
-    assign(:tickets, [
-      Ticket.create!(),
-      Ticket.create!()
-    ])
-  end
+# RSpec.describe "tickets/index", type: :view do
+#   before(:each) do
+#     assign(:tickets, [
+#       Ticket.create!(),
+#       Ticket.create!()
+#     ])
+#   end
 
-  it "renders a list of tickets" do
-    render
-  end
-end
+#   it "renders a list of tickets" do
+#     render
+#   end
+# end
